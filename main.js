73e1cbcc246f32f51f6d35a467e107d7d0d33852
nadasb <nadasaboula@gmail.com>
2025-09-04 01:05:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img1 = document.querySelector(".img1");
+let container = document.querySelector(".container")
+
+function phones(phone){
+    img1.src = phone;
+}
+
+function background(color){
+    container.style.background = color;
+}
